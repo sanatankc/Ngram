fdf381f0ad29f03b8467626ce31ff0918ae44c2f
Saurabh <Saurabh.19@outlook.com>
2017-06-16 09:40:46 +0530

    second working model with no dependency till now, and logically distributed sections. It have to add images and user accounts now.

--- app.js
+++ app.js
@@ -1,32 +1,18 @@
-// get the http api
-var http = require('http');
-// this is file system module
-var fs = require('fs')
+var http = require('http'); // get the http api
+var render = require('./render')  // all render handling here
 
 var server = http.createServer(function(req, res){
-    console.log('requested url: '+ req.url);
-    if (req.url == '/'){
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    // read the index page
-    var index = fs.createReadStream(__dirname+ '/frontend/index.html', 'utf-8');
-    index.pipe(res);
-  }
-  else if (req.url == '/css/reboot.css') {
-    // get the css
-    var style = fs.createReadStream(__dirname+ '/frontend/css/reboot.css')
-    // it will end the connection itself ;;
-    style.pipe(res);
-  }
-  else { /* do nothing just pass it */ }
+  console.log('requested url: '+ req.url);
+
+  if (req.url == '/')
+    render.home(req, res);
+  else if (req.url == '/favicon.ico')
+    res.end();
+  else if (req.url.search('css'))
+    render.style(req, res);
+  else
+    res.end();        //close connection
 });
 
 server.listen(3000, '127.0.0.1');
 console.log('Listening to http://127.0.0.1:3000\n');
-
-/*
-else if (req.url == '/css/reboot.css') {
-  console.log('requested url: '+ req.url);
-  var styles = fs.createReadStream(__dirname + './frontend/css/reboot.css');
-  res.writeHead(200, {'Content-Type': 'text/css'});
-  res.end(styles)
-*/
